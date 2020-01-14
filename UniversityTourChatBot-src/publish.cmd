9b5f66b700ee8a95c4d7372180bfee93f9274361
Hamroun Mohamed (DyP/ICO1) <hao1dy2@bosch.com>
2020-01-14 16:25:26 +0100

    premier commit du projet version 1

---
+++ UniversityTourChatBot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=universitytourchatbot-Web-Deploy.pubxml -p:Password=alerrYv9z0iiGhG1HyQ1AxgKANj632RqTLRnhLgwDshLNuNyE5fE9ESd7H4H
+
